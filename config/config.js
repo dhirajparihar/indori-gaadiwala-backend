fbdaa8fefe9d0b606e35465ef5a129d4289ab870
Dhiraj Parihar <dp601274@gmail.com>
2026-01-25 17:01:07 +0530

    changes newe

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-    PORT: process.env.PORT || 8080,
-    MONGODB_URI: process.env.MONGODB_URI || process.env.MONGO_URL || process.env.MONGODB_URL || process.env.DATABASE_URL,
+    PORT: process.env.PORT,
+    MONGODB_URI: process.env.MONGODB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRE: process.env.JWT_EXPIRE,
-    NODE_ENV: process.env.NODE_ENV || 'development',
+    NODE_ENV: process.env.NODE_ENV,
     CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
     CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
     CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET
